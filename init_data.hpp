98df0721dcaadbbc68e5d78e4dae244f120b5bc0
tou <dse2015@dses-MacBook-Pro.local>
2015-12-14 02:39:56 +0900

    add cluster data

--- init_data.hpp
+++ init_data.hpp
@@ -35,6 +35,41 @@ void randinit(std::vector<std::vector<double> > &points, int dimension, double l
   }  
 }
 
+void normal_init(std::vector<std::vector<double> > &points, int dimension, double mu, double sigma, double lower_bound, double upper_bound)
+{
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(mu,sigma);
+  // generator.
+  
+  for(int i = 0; i < points.size();i++){
+    std::vector<double> v;
+    for(int j = 0; j < dimension; j++){
+      double value = distribution(generator);
+      if(value > lower_bound && value < upper_bound)
+	v.push_back(value);
+      else
+	j--;
+    }
+    points[i] = v;
+  }  
+}
+
+void cluster_init(std::vector<std::vector<double> > &points, int dimension, double lower_bound, double upper_bound, double sigma)
+{
+  int center_num = 10;
+  int each_num = points.size() / center_num;
+  int current_index = 0;
+  for(int i = 0; i < center_num; i++){
+    double mu = get_rand(0.2,0.8);
+    std::vector<std::vector<double> > sub_points(each_num);
+    normal_init(sub_points,dimension,mu,sigma,lower_bound,upper_bound);
+    for(int j = 0; j < sub_points.size(); j++){
+      points[current_index] = sub_points[j];
+      current_index++;
+    }
+  }
+}
+
 // sum = 1
 void randinit_w(std::vector<std::vector<double> > &points, int dimension, double lower_bound, double upper_bound)
 {
@@ -65,14 +100,26 @@ void randinit_w(std::vector<std::vector<double> > &points, int dimension, double
 /*read data from file*/
 void read_point_file(std::vector<std::vector<double> > &points, int dimension, std::string path)
 {
-  std::ifstream ifs( path );
+  std::cout << path << std::endl;
+  std::ifstream ifs("/Users/dse2015/Desktop/Research/ARKR/data/houseD6.csv");
   std::vector<double> v((std::istream_iterator<double>(ifs)), std::istream_iterator<double>());
 
   int num = 0;
+  int count = 0;
   for(int i = 0; i < points.size();i++){
     for(int j = 0; j < dimension; j ++){
-      points[i].push_back(v[num++]);
+      if(v[num] > 10000)
+	points[i].push_back(get_rand(0,1));
+      else{
+	count++;
+	points[i].push_back(v[num]/10000);
+      }
+      num++;
     }
   }
+
+  
+  std::cout << "coount" << count << std::endl;
+  
 }
 
